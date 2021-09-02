ec7a6d7e937c21f0b7179693fe21f2bbdc82bad7
Alexandre DABE <alex.dabe2000@gmail.com>
2021-09-02 13:56:24 +0200

    Update temperature1b.py
    correction de la fonction sauvegarde

--- temperature1b.py
+++ temperature1b.py
@@ -18,7 +18,7 @@ def extraire_temperature (contenu) :
     return float(donnees_temperature[2:]) / 1000
 
 
-def sauvegarde(temperature_ext, temperature_int, tp, h, date, emplacement):
+def sauvegarde(temperature_ext, temperature_int, date, emplacement):
     with open(emplacement, 'a') as file:
         writer = csv.writer(file,delimiter=',',lineterminator='\n')
         #  headerList = ['t', 'te', 'ti']
@@ -30,14 +30,12 @@ def sauvegarde(temperature_ext, temperature_int, tp, h, date, emplacement):
         writer.writerow(data)
     file.close()
         
-
+        
 while True:
-    #  date = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     date = datetime.datetime.now()
     contenu_fichier1 = lire_fichier("/sys/bus/w1/devices/28-0114531c89aa/w1_slave")  # Lecture de la sonde exterieure
     contenu_fichier2 = lire_fichier("/sys/bus/w1/devices/28-0300a279c595/w1_slave")  # Lecture de la sonde interieure
     temperature_ext = extraire_temperature(contenu_fichier1)
-    temperature_int = extraire_temperature(contenu_fichier2)
-   
-    sauvegarde(temperature_ext, temperature_int, date, "Temp.csv")  # Appel de la fonction sauvegarde avec 3 variables
+    temperature_int = extraire_temperature(contenu_fichier2)   
+    sauvegarde(temperature_ext, temperature_int, date, "Temp.csv")  # Appel de la fonction sauvegarde avec 4 variables
     time.sleep(120)
